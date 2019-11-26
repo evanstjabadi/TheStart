501ff52026b38fabfd8f1e5dd7012184db8be0c9
Evans Tjabadi <evansltjabadi@gmail.com>
2019-11-26 17:02:33 +0200

    Everython added

--- python/hello-world.py
+++ python/hello-world.py
@@ -3,3 +3,16 @@
 # 26 November 2019
 
 print("Hello Git: 2019/Nov/26")
+
+a = 1
+b = 2
+c = "Robot"
+
+def func(n,m):
+    print(n, m)
+    print("We are done here")
+
+    return True
+
+w = func(2, 5)
+
